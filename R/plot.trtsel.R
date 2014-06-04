f1bce3f9141e16ee05410522b566c833381dacac
mdbrown <mdbrown@fhcrc.org>
2014-06-04 14:30:43 -0700

    bug in selection impact plot

--- R/plot.trtsel.R
+++ R/plot.trtsel.R
@@ -96,7 +96,7 @@ function(x, bootstraps = 500,
 
    if(substr(ci, 1,1 )=="v"){
        
-      if(is.element(substr(plot.type, 1,3), c("tre", "ris", "the"))) fixed.values = seq(from = 1, to = 100, length.out = conf.bandsN)
+      if(is.element(substr(plot.type, 1,3), c("tre", "ris", "sel"))) fixed.values = seq(from = 1, to = 100, length.out = conf.bandsN)
       else if(substr(plot.type, 1,3)=="cdf") fixed.values = seq(from = min(delta), to = max(delta), length.out = conf.bandsN)
       
    }else{
